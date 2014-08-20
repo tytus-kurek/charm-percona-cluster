c4d4a8e9e3a13f052018ff57bd1134954f5079d1
Liam Young <liam.young@canonical.com>
2014-08-20 08:42:15 +0000

    Sync charm helpers

--- hooks/charmhelpers/contrib/peerstorage/__init__.py
+++ hooks/charmhelpers/contrib/peerstorage/__init__.py
@@ -51,6 +51,24 @@ def peer_retrieve(key, relation_name='cluster'):
                          'peer relation {}'.format(relation_name))
 
 
+def peer_retrieve_by_prefix(prefix, relation_name='cluster', delimiter='_',
+                            inc_list=None, exc_list=None):
+    """ Retrieve k/v pairs given a prefix and filter using {inc,exc}_list """
+    inc_list = inc_list if inc_list else []
+    exc_list = exc_list if exc_list else []
+    peerdb_settings = peer_retrieve('-', relation_name=relation_name)
+    matched = {}
+    for k, v in peerdb_settings.items():
+        full_prefix = prefix + delimiter
+        if k.startswith(full_prefix):
+            new_key = k.replace(full_prefix, '')
+            if new_key in exc_list:
+                continue
+            if new_key in inc_list or len(inc_list) == 0:
+                matched[new_key] = v
+    return matched
+
+
 def peer_store(key, value, relation_name='cluster'):
     """ Store the key/value pair on the named peer relation relation_name """
     cluster_rels = relation_ids(relation_name)
@@ -86,11 +104,19 @@ def peer_echo(includes=None):
 
 
 def peer_store_and_set(relation_id=None, peer_relation_name='cluster',
-                       peer_store_fatal=False, relation_settings={}, **kwargs):
-    """ For each pair set them in the relation and store in peer db
+                       peer_store_fatal=False, relation_settings=None,
+                       delimiter='_', **kwargs):
+    """Store passed-in arguments both in argument relation and in peer storage.
 
-    Note that the relation set is done within the provided relation_id and
-    if none is provided defaults to the current relation"""
+    It functions like doing relation_set() and peer_store() at the same time,
+    with the same data.
+
+    @param relation_id: the id of the relation to store the data on. Defaults
+                        to the current relation.
+    @param peer_store_fatal: Set to True, the function will raise an exception
+                             should the peer sotrage not be avialable."""
+
+    relation_settings = relation_settings if relation_settings else {}
     relation_set(relation_id=relation_id,
                  relation_settings=relation_settings,
                  **kwargs)
@@ -98,7 +124,7 @@ def peer_store_and_set(relation_id=None, peer_relation_name='cluster',
         for key, value in dict(kwargs.items() +
                                relation_settings.items()).iteritems():
             key_prefix = relation_id or current_relation_id()
-            peer_store(key_prefix + '_' + key,
+            peer_store(key_prefix + delimiter + key,
                        value,
                        relation_name=peer_relation_name)
     else:
