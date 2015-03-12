6850829e453e7b6a9f63c08124d7ffc8b8d80957
James Page <james.page@ubuntu.com>
2015-03-12 12:27:47 +0000

    Deal with wonky in relation relation-sets

--- hooks/charmhelpers/contrib/peerstorage/__init__.py
+++ hooks/charmhelpers/contrib/peerstorage/__init__.py
@@ -60,7 +60,7 @@ def some_hook():
 
 def relation_set(relation_settings=None, relation_id=None, **kwargs):
     try:
-        if not relation_id or (relation_id in relation_ids('cluster')):
+        if relation_id in relation_ids('cluster'):
             return leader_set(settings=relation_settings, **kwargs)
         else:
             raise NotImplementedError
@@ -73,7 +73,7 @@ def relation_set(relation_settings=None, relation_id=None, **kwargs):
 
 def relation_get(attribute=None, rid=None, unit=None):
     try:
-        if not rid or (rid in relation_ids('cluster')):
+        if rid in relation_ids('cluster'):
             return leader_get(attribute)
         else:
             raise NotImplementedError
