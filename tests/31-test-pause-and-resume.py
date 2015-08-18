2fa6e9b71921b56ba80bfc39e87ea98685dcaf77
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-08-18 17:36:11 +0200

    Use charmhelpers implementations of run_action and wait_on_action.

--- tests/31-test-pause-and-resume.py
+++ tests/31-test-pause-and-resume.py
@@ -8,43 +8,19 @@ import time
 import json
 
 import basic_deployment
+from charmhelpers.contrib.amulet import AmuletUtils
 
+utils = AmuletUtils()
 
 class PauseResume(basic_deployment.BasicDeployment):
-
-    def _run_action(self, unit_id, action, *args):
-        command = ["juju", "action", "do", "--format=json", unit_id, action]
-        command.extend(args)
-        print("Running command: %s\n" % " ".join(command))
-        output = subprocess.check_output(command)
-        output_json = output.decode(encoding="UTF-8")
-        data = json.loads(output_json)
-        action_id = data[u'Action queued with id']
-        return action_id
-
-    def _wait_on_action(self, action_id):
-        command = ["juju", "action", "fetch", "--format=json", action_id]
-        while True:
-            try:
-                output = subprocess.check_output(command)
-            except Exception as e:
-                print(e)
-                return False
-            output_json = output.decode(encoding="UTF-8")
-            data = json.loads(output_json)
-            if data[u"status"] == "completed":
-                return True
-            elif data[u"status"] == "failed":
-                return False
-            time.sleep(2)
 
     def run(self):
         super(PauseResume, self).run()
         unit_name = "percona-cluster/0"
         unit = self.d.sentry.unit[unit_name]
         assert self.is_mysqld_running(unit=unit), "mysqld not running in initial state."
-        action_id = self._run_action(unit_name, "pause")
-        assert self._wait_on_action(action_id), "Pause action failed."
+        action_id = utils.run_action(self.d.sentry, "pause")
+        assert utils.wait_on_action(action_id), "Pause action failed."
 
         # Note that is_mysqld_running will print an error message when
         # mysqld is not running.  This is by design but it looks odd
@@ -53,8 +29,8 @@ class PauseResume(basic_deployment.BasicDeployment):
         init_contents = unit.directory_contents("/etc/init/")
         assert "mysql.override" in init_contents["files"], "Override file not created."
 
-        action_id = self._run_action(unit_name, "resume")
-        assert self._wait_on_action(action_id), "Resume action failed"
+        action_id = utils.run_action(self.d.sentry, "resume")
+        assert utils.wait_on_action(action_id), "Resume action failed"
         init_contents = unit.directory_contents("/etc/init/")
         assert "mysql.override" not in init_contents["files"], "Override file not removed."
         assert self.is_mysqld_running(unit=unit), "mysqld not running after resume."
