b1e7be4e008af344617599d2c12b48f13605adf0
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-08-19 17:37:01 +0200

    Try old wait on action

--- tests/31-test-pause-and-resume.py
+++ tests/31-test-pause-and-resume.py
@@ -12,7 +12,25 @@ from charmhelpers.contrib.amulet.utils import AmuletUtils
 
 utils = AmuletUtils()
 
+
+
 class PauseResume(basic_deployment.BasicDeployment):
+    def wait_on_action(self, action_id):
+        command = ["juju", "action", "fetch", "--format=json", action_id]
+        while True:
+            try:
+                output = subprocess.check_output(command) 
+            except Exception as e:
+                print(e)
+                return False
+            output_json = output.decode(encoding="UTF-8")                       
+            data = json.loads(output_json)
+            if data[u"status"] == "completed":
+                return True
+            elif data[u"status"] == "failed":
+                return False
+            time.sleep(2)
+
 
     def run(self):
         super(PauseResume, self).run()
@@ -20,7 +38,7 @@ class PauseResume(basic_deployment.BasicDeployment):
         unit = self.d.sentry.unit[unit_name]
         assert self.is_mysqld_running(unit=unit), "mysqld not running in initial state."
         action_id = utils.run_action(unit, "pause")
-        assert utils.wait_on_action(action_id), "Pause action failed."
+        assert self.wait_on_action(action_id), "Pause action failed."
 
         # Note that is_mysqld_running will print an error message when
         # mysqld is not running.  This is by design but it looks odd
