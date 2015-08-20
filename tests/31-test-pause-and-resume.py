e765c0c889435ebd8a7e0a54a4867c6c4ca0500a
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-08-20 14:31:03 +0200

    Removing looping

--- tests/31-test-pause-and-resume.py
+++ tests/31-test-pause-and-resume.py
@@ -17,24 +17,23 @@ utils = AmuletUtils()
 class PauseResume(basic_deployment.BasicDeployment):
 
     def run(self):
-        for i in range(self.units):
-            uid = 'percona-cluster/%d' % i
-            unit = self.d.sentry.unit[uid]
-            action_id = utils.run_action(unit, "pause")
-            assert utils.wait_on_action(action_id), "Pause action failed."
+        uid = 'percona-cluster/0'
+        unit = self.d.sentry.unit[uid]
+        action_id = utils.run_action(unit, "pause")
+        assert utils.wait_on_action(action_id), "Pause action failed."
 
-            # Note that is_mysqld_running will print an error message when
-            # mysqld is not running.  This is by design but it looks odd
-            # in the output.
-            assert not self.is_mysqld_running(unit=unit), "mysqld is still running!"
-            init_contents = unit.directory_contents("/etc/init/")
-            assert "mysql.override" in init_contents["files"], "Override file not created."
+        # Note that is_mysqld_running will print an error message when
+        # mysqld is not running.  This is by design but it looks odd
+        # in the output.
+        assert not self.is_mysqld_running(unit=unit), "mysqld is still running!"
+        init_contents = unit.directory_contents("/etc/init/")
+        assert "mysql.override" in init_contents["files"], "Override file not created."
 
-            action_id = utils.run_action(unit, "resume")
-            assert utils.wait_on_action(action_id), "Resume action failed"
-            init_contents = unit.directory_contents("/etc/init/")
-            assert "mysql.override" not in init_contents["files"], "Override file not removed."
-            assert self.is_mysqld_running(unit=unit), "mysqld not running after resume."
+        action_id = utils.run_action(unit, "resume")
+        assert utils.wait_on_action(action_id), "Resume action failed"
+        init_contents = unit.directory_contents("/etc/init/")
+        assert "mysql.override" not in init_contents["files"], "Override file not removed."
+        assert self.is_mysqld_running(unit=unit), "mysqld not running after resume."
 
 
 if __name__ == "__main__":
