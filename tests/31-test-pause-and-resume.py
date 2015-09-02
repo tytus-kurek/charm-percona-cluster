05282a318e58233b4e47b67f641585082775e45e
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-09-02 17:13:15 +0200

    experimental pause

--- tests/31-test-pause-and-resume.py
+++ tests/31-test-pause-and-resume.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # test percona-cluster pause and resum
 
+import time
+
 import basic_deployment
 from charmhelpers.contrib.amulet.utils import AmuletUtils
 from charmhelpers.core.hookenv import status_get
@@ -30,7 +32,9 @@ class PauseResume(basic_deployment.BasicDeployment):
         init_contents = unit.directory_contents("/etc/init/")
         assert "mysql.override" in init_contents["files"], \
             "Override file not created."
-        # assert status_get()[0] == "maintenance"
+
+        time.sleep(10)
+        assert status_get()[0] == "maintenance"
         action_id = utils.run_action(unit, "resume")
         assert utils.wait_on_action(action_id), "Resume action failed"
         assert status_get()[0] == "active"
