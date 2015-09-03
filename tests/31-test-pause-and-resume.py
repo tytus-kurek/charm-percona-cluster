62ba6c9a342a16f9da0861e416e2c1020e3d1ff2
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-09-03 17:57:10 +0200

    Unpack status and return code.

--- tests/31-test-pause-and-resume.py
+++ tests/31-test-pause-and-resume.py
@@ -7,7 +7,10 @@ from charmhelpers.contrib.amulet.utils import AmuletUtils
 
 def status_get(unit):
     import json
-    return unit.run("status-get --format=json")
+    raw_status, return_code = unit.run("status-get --format=json")
+    status = json.loads(raw_status.decode("UTF-8"))
+    return (status["status"], status["message"])
+        
     # status = json.loads(raw_status.decode("UTF-8"))
     # return (status["status"], status["message"])
 
