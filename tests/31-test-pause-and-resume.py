5f4eed3500097bd46a4ebc195f7c0e8dc54bc7dc
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-09-03 17:45:26 +0200

    Debugging

--- tests/31-test-pause-and-resume.py
+++ tests/31-test-pause-and-resume.py
@@ -30,7 +30,8 @@ class PauseResume(basic_deployment.BasicDeployment):
         uid = 'percona-cluster/0'
         unit = self.d.sentry.unit[uid]
         assert self.is_mysqld_running(unit), 'mysql not running: %s' % uid
-        assert status_get(unit)[0] == "unknown"
+        print status_get(unit)
+        assert  status_get(unit)[0] == "unknown"
         
         action_id = utils.run_action(unit, "pause")
         assert utils.wait_on_action(action_id), "Pause action failed."
